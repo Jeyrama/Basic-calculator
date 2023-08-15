77d3f5b4656cab0d81f25b4834e6ec9e8380e95b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-14 17:09:47 -0700

    Update Basic-calculator.js

--- Basic-calculator.js
+++ Basic-calculator.js
@@ -35,3 +35,14 @@ function calculate(num1, operation, num2) {
 }
 
 // or
+
+function calculate(num1, operation, num2) {
+  if (operation !== '*' &&  operation !== '+' &&  operation !== '-' && num2 === 0) return null
+  else
+  switch (operation) {
+     case '+': return num1 + num2;
+     case '-': return num1 - num2;
+     case '/': return num1 / num2;
+     case '*': return num1 * num2;
+ }
+}
